ed64cb42da8a5b7fe0bce48165dfe2b552fa77fb
Julian Florez <forezjuli08@hotmail.com>
2024-08-08 14:45:15 -0500

    update - use os.Stdin as reader for Games 'New' method

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/juliflorezg/go-pocket-projects-gordle/gordle"
+import (
+	"os"
+
+	"github.com/juliflorezg/go-pocket-projects-gordle/gordle"
+)
 
 func main() {
-	g := gordle.New()
+	g := gordle.New(os.Stdin)
 
 	g.Play()
 }
