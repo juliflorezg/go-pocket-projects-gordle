f759f37f42fe37edf6e4291c78bc8bec845c8bcb
Julian Florez <forezjuli08@hotmail.com>
2024-08-14 09:50:20 -0500

    add - tests for validate guess and input normalization functions

--- gordle/game_rename_test.go
+++ gordle/game_rename_test.go
@@ -1,6 +1,7 @@
 package gordle
 
 import (
+	"errors"
 	"slices"
 	"strings"
 	"testing"
@@ -37,6 +38,76 @@ func TestGameAsk(t *testing.T) {
 			got := g.ask()
 			if !slices.Equal(got, tc.want) {
 				t.Errorf("got = %v | want =%v ", string(got), string(tc.want))
+			}
+		})
+	}
+}
+
+func TestGameValidateGuess(t *testing.T) {
+	tt := map[string]struct {
+		input []rune
+		want  error
+	}{
+		"correct length": {
+			input: []rune("hello"),
+			want:  nil,
+		},
+		"length too short": {
+			input: []rune("hel"),
+			want:  errInvalidWordLength,
+		},
+		"length too long": {
+			input: []rune("hellooo"),
+			want:  errInvalidWordLength,
+		},
+		"empty input": {
+			input: []rune(""),
+			want:  errInvalidWordLength,
+		},
+		"no input": {
+			input: nil,
+			want:  errInvalidWordLength,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			g := New(strings.NewReader(string(tc.input)))
+
+			got := g.validateGuess(tc.input)
+
+			if !errors.Is(got, tc.want) {
+				t.Errorf("word: %c => got: %v | want: %v", tc.input, got, tc.want)
+			}
+		})
+	}
+}
+
+func TestSplitToUppercaseCharacters(t *testing.T) {
+	tt := map[string]struct {
+		input string
+		want  []rune
+	}{
+		"all uppercase": {
+			input: "UPPER",
+			want:  []rune("UPPER"),
+		},
+		"all lowercase": {
+			input: "lower",
+			want:  []rune("LOWER"),
+		},
+		"upper and lower": {
+			input: "UPper LowER",
+			want:  []rune("UPPER LOWER"),
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			got := splitToUppercaseCharacters(tc.input)
+
+			if !slices.Equal(got, tc.want) {
+				t.Errorf("got: %c | want: %c", got, tc.want)
 			}
 		})
 	}
